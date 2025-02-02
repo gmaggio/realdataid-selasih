6c624d5e082e2847b85e86decf7cd154be070987
gmaggio <grld.mgio@yahoo.com>
2025-02-02 20:57:41 +0700

    ref(sidebar): tidy up

--- src/shared/components/Sidebar/index.tsx
+++ src/shared/components/Sidebar/index.tsx
@@ -54,107 +54,46 @@ const Sidebar: React.FC = () => {
           'drop-shadow-[4px_0px_4px_rgba(0,_0,_0,_0.1)]',
         )}
       >
-        <label
-          htmlFor="sidemenu-toggle"
-          id="sidemenu-toggle-collapse"
-          className={clsx(
-            'absolute',
-            'right-0 translate-x-1/2',
-            'top-[5.9375rem]',
-            'z-150 cursor-pointer',
-          )}
-        >
-          <input
-            id="sidemenu-toggle"
-            type="checkbox"
-            className={clsx('peer hidden')}
-          />
-
-          <div
-            className={clsx(
-              'bg-btnPrimary',
-              'p-1',
-              'rounded-full',
-              'size-6 text-txtOnDark',
-              'hover:ring-6 hover:ring-btnSecondary',
-              'rotate-0 peer-checked:rotate-180',
-              'transition-transform',
-            )}
-          >
-            <ChevronLeftIcon />
-          </div>
-        </label>
-
+        <SidebarToggler />
         <SidebarHeader />
-
-        <div
-          className={clsx(
-            'flex flex-col gap-4',
-            'flex-1',
-            'w-full h-dvh',
-            'px-3.5 pb-4',
-            'overflow-y-scroll',
-          )}
-        >
-          <nav className={clsx('flex-1 h-auto')}>
-            <SidebarMenu
-              items={[
-                <SidebarMenuItem
-                  key="ajukan-sertifikasi"
-                  href=""
-                  icon={DocumentTextIcon}
-                  variants={{ type: 'hilite' }}
-                >
-                  Ajukan Sertifikasi
-                </SidebarMenuItem>,
-                <SidebarMenuItem key="beranda" href="" icon={HomeIcon}>
-                  Beranda
-                </SidebarMenuItem>,
-                <SidebarMenuItem key="pembayaran" href="" icon={CreditCardIcon}>
-                  Pembayaran
-                </SidebarMenuItem>,
-                <SidebarMenuItem
-                  key="proses-sertifikasi"
-                  href="/"
-                  icon={CheckBadgeIcon}
-                >
-                  Proses Sertifikasi
-                </SidebarMenuItem>,
-                <SidebarMenuItem
-                  key="bantuan"
-                  href=""
-                  icon={QuestionMarkCircleIcon}
-                >
-                  Bantuan
-                </SidebarMenuItem>,
-              ]}
-            />
-          </nav>
-          <nav className={clsx('pt-4.5', 'border-t border-lineSecondary')}>
-            <SidebarMenu
-              items={[
-                <SidebarMenuItem
-                  key="pengaturan-profil"
-                  href=""
-                  icon={UserIcon}
-                >
-                  Pengaturan Profil
-                </SidebarMenuItem>,
-                <SidebarMenuItem
-                  key="keluar"
-                  href=""
-                  icon={ArrowRightStartOnRectangleIcon}
-                >
-                  Keluar
-                </SidebarMenuItem>,
-              ]}
-            />
-          </nav>
-        </div>
+        <SideBarMenus />
       </aside>
     </div>
   );
 };
+
+const SidebarToggler: React.FC = () => (
+  <label
+    htmlFor="sidemenu-toggle"
+    id="sidemenu-toggle-collapse"
+    className={clsx(
+      'absolute',
+      'right-0 translate-x-1/2',
+      'top-[5.9375rem]',
+      'z-150 cursor-pointer',
+    )}
+  >
+    <input
+      id="sidemenu-toggle"
+      type="checkbox"
+      className={clsx('peer hidden')}
+    />
+
+    <div
+      className={clsx(
+        'bg-btnPrimary',
+        'p-1',
+        'rounded-full',
+        'size-6 text-txtOnDark',
+        'hover:ring-6 hover:ring-btnSecondary',
+        'rotate-0 peer-checked:rotate-180',
+        'transition-transform',
+      )}
+    >
+      <ChevronLeftIcon />
+    </div>
+  </label>
+);
 
 const SidebarHeader: React.FC = () => {
   return (
@@ -195,5 +134,64 @@ const SidebarHeader: React.FC = () => {
     </div>
   );
 };
+
+const SideBarMenus: React.FC = () => (
+  <div
+    className={clsx(
+      'flex flex-col gap-4',
+      'flex-1',
+      'w-full h-dvh',
+      'px-3.5 pb-4',
+      'overflow-y-scroll',
+    )}
+  >
+    <nav className={clsx('flex-1 h-auto')}>
+      <SidebarMenu
+        items={[
+          <SidebarMenuItem
+            key="ajukan-sertifikasi"
+            href=""
+            icon={DocumentTextIcon}
+            variants={{ type: 'hilite' }}
+          >
+            Ajukan Sertifikasi
+          </SidebarMenuItem>,
+          <SidebarMenuItem key="beranda" href="" icon={HomeIcon}>
+            Beranda
+          </SidebarMenuItem>,
+          <SidebarMenuItem key="pembayaran" href="" icon={CreditCardIcon}>
+            Pembayaran
+          </SidebarMenuItem>,
+          <SidebarMenuItem
+            key="proses-sertifikasi"
+            href="/"
+            icon={CheckBadgeIcon}
+          >
+            Proses Sertifikasi
+          </SidebarMenuItem>,
+          <SidebarMenuItem key="bantuan" href="" icon={QuestionMarkCircleIcon}>
+            Bantuan
+          </SidebarMenuItem>,
+        ]}
+      />
+    </nav>
+    <nav className={clsx('pt-4.5', 'border-t border-lineSecondary')}>
+      <SidebarMenu
+        items={[
+          <SidebarMenuItem key="pengaturan-profil" href="" icon={UserIcon}>
+            Pengaturan Profil
+          </SidebarMenuItem>,
+          <SidebarMenuItem
+            key="keluar"
+            href=""
+            icon={ArrowRightStartOnRectangleIcon}
+          >
+            Keluar
+          </SidebarMenuItem>,
+        ]}
+      />
+    </nav>
+  </div>
+);
 
 export default Sidebar;
